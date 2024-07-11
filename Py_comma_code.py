38fc5964e4cff29e22a7ea808deb52f446d873b9
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-11 14:31:13 +0500

    Changes to 'Py_comma_code.py'

--- Py_comma_code.py
+++ Py_comma_code.py
@@ -1,32 +1,21 @@
-""" spam= ['apples', 'bananas', 'tofu', 'cats']
-def list_thing(list):
-    new_string = ''
-    for i in list:
-        new_string = new_string + str(i)
-        if list.index(i) == (len(list)-2):
-            new_string = new_string + ', and '
-        elif list.index(i) == (len(list)-1):
-            new_string = new_string
-        else:
-            new_string = new_string + ', '
-    return new_string
-
-print (list_thing(spam)) """
-
-# couldnt code this shit took it from Stack Overflow
-
 
 def commaCode():
     items = []
     while True:
-        print('Enter an item' + ' (Or enter nothing to stop.):')
+        print('Enter an item (Or enter nothing to stop):')
         name = input()
         if name == '':
             break
         items = items + [name]
     print('Items are:')
     for name in items:
-        print(name, end=', ')
+        
+        if items.index(name) == (len(items) - 2 ):
+            print("and")
+        elif items.index(name) == (len(items) - 1):
+            print(name)
+        else:
+            print(name, end= ", ")
 
 
 commaCode()
