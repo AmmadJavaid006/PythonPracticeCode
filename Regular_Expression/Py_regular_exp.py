ebcba3d4915a2b71f0339487afd97c4f76a106cc
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-14 16:27:44 +0500

    New Folder For Regular Expression & Code

---
+++ Regular_Expression/Py_regular_exp.py
@@ -0,0 +1,20 @@
+import re
+count = 0
+count2 = 0
+
+fname = input("Enter File Name: ")
+fhandle = open(fname)
+for line in fhandle:
+    if line.startswith("From") and not line.startswith("From:"):
+        print(line)
+        count += 1
+print(count)
+
+
+fname = input("Enter File Name: ")
+fhandle = open(fname)
+for line in fhandle:
+    if re.search("^From", line) and not re.search("^From:", line):
+        print(line)
+        count2 += 1
+print(count2)
