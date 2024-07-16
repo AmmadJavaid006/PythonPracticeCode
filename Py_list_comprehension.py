5e2471df4313f3c92e0532199d24207968979c9c
AmmadJavaid006 <ammad.javaid420@gmail.com>
2024-07-17 00:21:55 +0500

    Little Change

--- Py_list_comprehension.py
+++ Py_list_comprehension.py
@@ -1,73 +1,73 @@
-""" end = int(input("Till What Number U Want Sqr: "))
+end = int(input("Till What Number U Want Sqr: "))
 sqrr = [number for number in range(1, end + 1)]
 sqr = [sqrnum**2 for sqrnum in sqrr] # Calculate sqr (**2) of nums
-print(sqr) """
+print(sqr)
 
 #########################################################################################
 
-""" end = int(input("Till What Number U Want All Even Numbers: "))
+end = int(input("Till What Number U Want All Even Numbers: "))
 num = [number for number in range(1, end + 1)]
 nums = [int(numz) for numz in num]
 even = [number for number in nums if number % 2 == 0]
-print(even) """
+print(even)
 
 #########################################################################################
 
-""" end = int(input('Till What Number U Want to Replace Even No. with "Even": '))
+end = int(input('Till What Number U Want to Replace Even No. with "Even": '))
 repeven = ["Even" if number % 2 == 0 else number for number in range(1, end + 1)]
-print(repeven) """
+print(repeven)
 
 #########################################################################################
 
-""" end = int(input("Enter: "))
+end = int(input("Enter: "))
 list = [number for number in range(1, end + 1)]
 sqrnum = [(sqrnums, sqrnums**2) for sqrnums in list]
-print(sqrnum) """
+print(sqrnum)
 
 #########################################################################################
 
-""" finallist = [[i + z*3 + 1 for i in range(3)] for z in range(3)]
+finallist = [[i + z*3 + 1 for i in range(3)] for z in range(3)]
 for row in finallist:
-    print(row, end = ",\n") """
+    print(row, end = ",\n")
 
 #########################################################################################
 
-""" list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 flatlist = [elements for element in list for elements in element]
-print(flatlist) """
+print(flatlist)
 
 #########################################################################################
 
-""" words = ["apple", "banana", "cherry"]
+words = ["apple", "banana", "cherry"]
 list = [len(item) for item in words] 
-print(list) """
+print(list)
 
 #########################################################################################
 
-""" sentence = "The quick brown fox jumps over the lazy dog"
+sentence = "The quick brown fox jumps over the lazy dog"
 list = [len(word) for word in sentence.split()]
-print(list) """
+print(list)
 
 #########################################################################################
 
-""" end = int(input("Enter Number Of Digits: "))
+end = int(input("Enter Number Of Digits: "))
 list = [number for number in range(1, end + 1)]
 finallist = [number for number in list if number % 3 == 0]
-print(finallist) """
+print(finallist)
 
 #########################################################################################
 
-""" list = ['a', 'b', 'c', 'd']
+list = ['a', 'b', 'c', 'd']
 
 tuplelist = [(list.index(index), index) for index in list]
-print(tuplelist) """
+print(tuplelist)
 
 #########################################################################################
 
-""" n = int(input("Value for NxN is: "))
+n = int(input("Value for NxN is: "))
 matrix = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
 for i in matrix:
-    print(i) """
+    print(i)
 
 #########################################################################################
 
